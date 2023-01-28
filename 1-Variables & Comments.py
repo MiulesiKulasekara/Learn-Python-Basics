35f9b2ffc0713b115ec3603fbb0ab7e8bc5585ac
MiulesiKulasekara <it21028878@my.sliit.lk>
2023-01-28 20:09:04 +0530

    Variables & Comments - completed

---
+++ 1-Variables & Comments.py
@@ -0,0 +1,23 @@
+
+# Variables
+
+name = "Shamal" 
+age = 22 
+name1 = "Jhon"
+HEIGHT = 1.3
+my_town = "Newyork"
+_school = "High school"
+
+print(name)
+print(age) ; print(name1)
+print(HEIGHT)
+print(my_town);print(_school)
+
+# Following names can not use as variable names
+    # 123 = "hi"
+    # test! = 34
+    # keywords are not allowed to use as variable names
+
+# Comments are denoted by hashtag
+
+#this is a comment
