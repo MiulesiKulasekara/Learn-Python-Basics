12ce16bafb21bb2bb57153a4cb6024525dbc1e34
MiulesiKulasekara <it21028878@my.sliit.lk>
2023-01-31 12:03:27 +0530

    Control structure - completed

---
+++ 5-Control Structure.py
@@ -0,0 +1,14 @@
+# Control Structure
+
+pet = "Bird"
+
+if pet == "Cat":
+    print("I have a cat.It's name is Kitty.")
+elif pet == "Dog":
+    print("I have a dog.It's name is Tommy.")
+elif pet == "Parrot":
+    print("I have a parrot.It's name is Pully.")
+elif pet == "Rabbit":
+    print("I have a rabbit.It's name is Bobbie")
+else:
+    print("I have a pet")
